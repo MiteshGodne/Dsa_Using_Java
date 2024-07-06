35c0985701269555c9e53666eb8bfb8ad4142299
Mitesh <dev.miteshgodne@gmail.com>
2024-07-06 22:22:35 +0530

    All Divisors

--- _2_NumberSeries/Divisors.java
+++ _2_NumberSeries/Divisors.java
@@ -4,17 +4,34 @@ import java.util.Scanner;
 
 public class Divisors {
   // Time Complexity is :- (sqrt(n)) = 0(sqrt(n)+sqrt(n))
-  public static void printDivisor(int num) {
+  public static void printDivisorSorted(int num) {
     int i;
     for (i = 1; i * i < num; i++) {
       if (num % i == 0) {
         System.out.print(i + " ");
       }
     }
+    // seperating loops to display result in ascending order.
     for (; i >= 1; i--) {
       if (num % i == 0) {
         System.out.print(num / i + " ");
       }
+    }
+  }
+
+  public static void printDivisorUnsorted(int num) {
+    int count = 1;
+    System.out.print("All divisors are - ");
+    while (count * count <= num) {
+      if (num % count == 0) {
+        System.out.print(count + " ");
+        if (count == num / count) {
+
+        } else {
+          System.out.print(num / count + " ");
+        }
+      }
+      count++;
     }
   }
 
@@ -23,6 +40,6 @@ public class Divisors {
     @SuppressWarnings("resource")
     Scanner scan = new Scanner(System.in);
     int num = scan.nextInt();
-    printDivisor(num);
+    printDivisorUnsorted(num);
   }
 }

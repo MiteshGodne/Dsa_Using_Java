9330cedefd11d609751d138b7e7c832b1ac81efd
Mitesh <dev.miteshgodne@gmail.com>
2024-07-08 00:29:08 +0530

    Convert character into Upper Case or Lower Case Using bits manipulation

--- _5_BitsManipulation/CharCaseConversion.java
+++ _5_BitsManipulation/CharCaseConversion.java
@@ -23,13 +23,15 @@ public class CharCaseConversion {
 
       if (letter >= 97 && letter < 123) {
         char capital = (char) toUppercase(letter);
-        System.out.printf(">> Upper case of '%c' is '%c' \n", letter, capital);
+        System.out.printf("\n>> Upper case of '%c' is '%c' \n", letter, capital);
       } else if (letter >= 65 && letter < 91) {
         char small = (char) toLowercase(letter);
-        System.out.printf(">> Lower case of '%c' is '%c' \n", letter, small);
+        System.out.printf("\n>> Lower case of '%c' is '%c' \n", letter, small);
       } else {
         System.out.println(">> It's not an alphabet !!\n");
       }
+    } catch (Exception e) {
+      System.out.println("\nError >> Invalid Input !!");
     }
   }
 }

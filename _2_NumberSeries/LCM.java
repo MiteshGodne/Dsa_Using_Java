2644b87b100bd49ee1cf08516a033f2f104eb361
Mitesh <dev.miteshgodne@gmail.com>
2024-07-07 12:12:14 +0530

    Least Common Multiple

--- _2_NumberSeries/LCM.java
+++ _2_NumberSeries/LCM.java
@@ -1,16 +1,16 @@
 package _2_NumberSeries;
+
 import java.util.Scanner;
+
+import _4_MathAlgorithms.EuclidVsGabrielLameAlgo;
 
 public class LCM {
 
   // Brute force method
   public static void lcm(int num1, int num2) {
     int max = Math.max(num1, num2);
-    int count = 0;
     for (int i = max; i <= num1 * num2; i++) {
-      count++;
       if (i % num1 == 0 && i % num2 == 0) {
-      System.out.println(count);
         System.out.println("LCM is " + i);
         return;
       }
@@ -24,9 +24,11 @@ public class LCM {
     int num1 = scan.nextInt();
     System.out.print("Enter a number : ");
     int num2 = scan.nextInt();
+
+    // Brute Force
     lcm(num1, num2);
 
     // Euclid's Algorithm
-    System.out.println((num1*num2)/(GCD.gabrielGcd(num1,num2)));
+    System.out.println("LCM is " + (num1 * num2) / (EuclidVsGabrielLameAlgo.gabrielGcd(num1, num2)));
   }
 }

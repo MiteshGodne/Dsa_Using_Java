29ffb8feae60cf789d4cb7e5781e569beb16d586
Mitesh <dev.miteshgodne@gmail.com>
2024-07-10 00:00:14 +0530

    Odd Or Even Using Bit Operations

---
+++ _5_BitsManipulation/OddOrEven.java
@@ -0,0 +1,21 @@
+package _5_BitsManipulation;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+  public static void oddOrEven(int num) {
+    if (NthBit.nthBit(num, 0))
+      System.out.printf("%d is odd", num);
+    else
+      System.out.printf("%d is even", num);
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("Enter the number you want to check for Odd or Even : ");
+      int num = sc.nextInt();
+      oddOrEven(num);
+    }
+  }
+}

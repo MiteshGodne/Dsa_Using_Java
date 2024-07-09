f45ea1ee787d52fad5c2a0e79e6d79642e037c8c
Mitesh <dev.miteshgodne@gmail.com>
2024-07-09 23:52:43 +0530

    Nth bit of binary equivalent of any number

--- _5_BitsManipulation/NthBit.java
+++ _5_BitsManipulation/NthBit.java
@@ -4,18 +4,22 @@ package _5_BitsManipulation;
 import java.util.Scanner;
 
 public class NthBit {
-  public static int nthBit(int num, int bit) {
+  public static boolean nthBit(int num, int bit) {
     int res = num & (1 << bit);
-    return (res == 0 ? 0 : 1);
+    return (res == 0 ? false: true);
   }
 
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
-      System.out.print("Enter the binary number : ");
+      System.out.print("Enter any decimal number : ");
       int num = sc.nextInt();
-      System.out.print("Enter nth bit you want to find : ");
+      System.out.print("Enter nth bit you want to find from its binary equivalent : ");
       int bit = sc.nextInt();
-      System.out.print("Nth bit is " + nthBit(num, bit));
+      if(nthBit(num, bit)){
+        System.out.print("Nth bit is 1");
+      }else{
+        System.out.print("Nth bit is 0");
+      }
     }
   }
 }

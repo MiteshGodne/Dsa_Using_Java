6154b9160ffe30ef9dd84deae47c312ad7747f84
Mitesh <dev.miteshgodne@gmail.com>
2024-07-11 23:48:38 +0530

    Swap two numbers using bit's X-OR operation

--- _5_BitsManipulation/SwapTwoNumbers.java
+++ _5_BitsManipulation/SwapTwoNumbers.java
@@ -7,12 +7,14 @@ public class SwapTwoNumbers {
     // Properties Used -
     // 1] a ^ a = 0
     // 2] a ^ 0 = a
+    // X-OR is associative - a^b^c == b^a^c
 
     num1 = num1 ^ num2;
     num2 = num1 ^ num2; // num2 = (num1 ^ num2) ^ num2 => num1 ^ 0 => num1
     num1 = num1 ^ num2; // num1 = (num1 ^ num2) ^ (num1) => 0 ^ num2 => num2
 
-    System.out.printf("Value after swap :- \nFirst Number = %d and Second Number = %d", num1, num2);
+    System.out.printf("Values after swap :- \nFirst Number = %d, Second Number = %d", num1, num2);
+    System.out.println("\n");
   }
 
   public static void main(String[] args) {
@@ -21,8 +23,9 @@ public class SwapTwoNumbers {
       int num1 = sc.nextInt();
       System.out.print("Enter the value of second number : ");
       int num2 = sc.nextInt();
+      System.out.println("\n");
 
-      System.out.printf("Value before swap :- \nFirst Number = %d and Second Number = %d\n", num1, num2);
+      System.out.printf("Values before swap :- \nFirst Number = %d, Second Number = %d\n", num1, num2);
       swapUsingBits(num1, num2);
     }
   }

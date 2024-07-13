7670db589fad2b1522f4a8408baa66aed8b0b9d4
Mitesh <dev.miteshgodne@gmail.com>
2024-07-13 14:02:01 +0530

    Insert an element anywhere into an array

--- _6_Arrays/InsertIntoArray.java
+++ _6_Arrays/InsertIntoArray.java
@@ -4,16 +4,11 @@ import java.util.Scanner;
 
 public class InsertIntoArray {
   public static void insertIntoArray(int[] arr, int posi, int element) {
-    if (posi < 0 && posi > arr.length - 1) {
-      System.out.println("You entered invalid position !!");
-      return;
-    } else {
-      // Shifting of elements towards the end of the array
-      for (int i = arr.length - 2; i >= posi; i--) {
-        arr[i + 1] = arr[i];
-      }
-      arr[posi] = element;
+    // Shifting of elements towards the end of the array
+    for (int i = arr.length - 2; i >= posi; i--) {
+      arr[i + 1] = arr[i];
     }
+    arr[posi] = element;
   }
 
   public static void main(String[] args) {
@@ -24,11 +19,11 @@ public class InsertIntoArray {
 
       int arr[] = new int[size];
       for (int i = 0; i < arr.length; i++) {
-        if(i==arr.length-1){
-          System.out.print("Note : If you add last element and then insert another element in between, last element will be lost. To continue enter Y : ");
+        if (i == arr.length - 1) {
+          System.out.print(
+              "Note : If you add last element and then insert another element in between, last element will be lost. To leave it blank enter Y and to continue press any other key : ");
           char choice = sc.next().charAt(0);
-          if(choice == 'Y' || choice == 'y'){}
-          else{
+          if (choice == 'Y' || choice == 'y') {
             break;
           }
         }
@@ -38,16 +33,17 @@ public class InsertIntoArray {
 
       System.out.print("Enter the position after which you want to add the element : ");
       int posi = sc.nextInt();
-
-      System.out.print("Enter the element you want to insert : ");
-      int ele = sc.nextInt();
-
-      ArrayUtility.printIntegerArray(arr);
-      insertIntoArray(arr, posi, ele);
-      System.out.println("\n");
-      ArrayUtility.printIntegerArray(arr);
-    }
-    catch(Exception e){
+      if (posi < 0 || posi > arr.length - 1) {
+        System.out.println("You entered invalid position !!");
+      } else {
+        System.out.print("Enter the element you want to insert : ");
+        int ele = sc.nextInt();
+        ArrayUtility.printIntegerArray(arr);
+        insertIntoArray(arr, posi, ele);
+        System.out.println("\n");
+        ArrayUtility.printIntegerArray(arr);
+      }
+    } catch (Exception e) {
       System.out.println("Invalid Input !!");
     }
   }

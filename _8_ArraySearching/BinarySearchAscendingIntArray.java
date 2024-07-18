3f38c6ba9079845e66cbc247893cfac2b288fa2e
Mitesh <dev.miteshgodne@gmail.com>
2024-07-19 00:08:41 +0530

    Finding target element using binary search in an ascending array

--- _8_ArraySearching/BinarySearchAscendingIntArray.java
+++ _8_ArraySearching/BinarySearchAscendingIntArray.java
@@ -7,7 +7,7 @@ import _6_Arrays.ArrayUtility;
 public class BinarySearchAscendingIntArray {
 
   // ( start + end ) / 2 == start + ((end - start) / 2))
-  static int binarySearchIntegerArray(int arr[], int target) {
+  static int binarySearchAscendingIntArray(int arr[], int target) {
     int start = 0, end = arr.length - 1, mid;
     while (start <= end) {
       mid = start + ((end - start) / 2);
@@ -31,7 +31,7 @@ public class BinarySearchAscendingIntArray {
       int arr[] = ArrayUtility.inputIntArray(size);
       System.out.print("Enter the target element to search : ");
       int target = sc.nextInt();
-      int index = binarySearchIntegerArray(arr, target);
+      int index = binarySearchAscendingIntArray(arr, target);
 
       if (index < 0) {
         System.out.println("\n>> Element is not present in the array.");

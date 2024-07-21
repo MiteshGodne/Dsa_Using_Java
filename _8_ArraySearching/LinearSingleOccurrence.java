6f8d9120368b6eb07368ffb169b2c46b2b84dfa1
Mitesh <dev.miteshgodne@gmail.com>
2024-07-21 23:49:02 +0530

    Fetch First or Last Occurance of an element from an array based on user's choice

---
+++ _8_ArraySearching/LinearSingleOccurrence.java
@@ -0,0 +1,53 @@
+package _8_ArraySearching;
+
+import java.util.Scanner;
+
+import _6_Arrays.ArrayUtility;
+
+public class LinearSingleOccurrence {
+  public static int linearSearch(int[] array, int target, boolean findLast) {
+    int occurrence = -1;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == target) {
+        if (findLast == false) {
+          occurrence = i;
+          return occurrence;
+        } else {
+          occurrence = i;
+        }
+      }
+    }
+    return occurrence;
+  }
+
+  public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.print("Enter the array size : ");
+      int size = scanner.nextInt();
+
+      int array[] = ArrayUtility.inputIntArray(size);
+
+      System.out.print("\nEnter the target element you want to search : ");
+      int target = scanner.nextInt();
+
+      System.out.print("\nEnter Y/y if you want the last occurrence of target : ");
+      char last = scanner.next().charAt(0);
+      boolean findLast = false;
+
+      if (last == 'Y' || last == 'y') {
+        findLast = true;
+      }
+
+      int index = linearSearch(array, target, findLast);
+
+      if (index >= 0) {
+        System.out.printf("\nElement found at position %d\n", index + 1);
+      } else {
+        System.out.print("\nElement Not Found !!\n");
+      }
+    } catch (Exception e) {
+      System.out.println("\n Error >> Invalid Input !!\n");
+    }
+  }
+}

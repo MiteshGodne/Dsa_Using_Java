24ad3df81e632839ac4025a9d101144adcb5c0b5
Mitesh <dev.miteshgodne@gmail.com>
2024-07-21 23:57:08 +0530

    Fetch all the positions where target element is found

---
+++ _8_ArraySearching/LinearMultipleOccurrence.java
@@ -0,0 +1,50 @@
+package _8_ArraySearching;
+
+import java.util.Scanner;
+
+import _6_Arrays.ArrayUtility;
+
+public class LinearMultipleOccurrence {
+
+  public static void multipleOccurrences(int[] array, int target) {
+    int[] positions = new int[array.length];
+    int j = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == target) {
+        positions[j] = i;
+      }
+      j++;
+    }
+
+    if (j != 0) {
+      // To Print the positions
+      System.out.print("\nPositions where target element is found are : ");
+      for (int i = 0; i < j; i++) {
+        if (i == --j)
+          System.out.print(i + " ");
+        else
+          System.out.print(i + ", ");
+      }
+    } else {
+      System.out.println("\nElement not found !!");
+    }
+  }
+
+  public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.print("Enter the array size : ");
+      int size = scanner.nextInt();
+
+      int array[] = ArrayUtility.inputIntArray(size);
+
+      System.out.print("\nEnter the target element you want to search : ");
+      int target = scanner.nextInt();
+
+      multipleOccurrences(array, target);
+
+    } catch (Exception e) {
+      System.out.println("\n Error >> Invalid Input !!\n");
+    }
+  }
+}

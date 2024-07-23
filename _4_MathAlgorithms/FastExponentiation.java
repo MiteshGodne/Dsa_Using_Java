237837d8da7c01d0f3549023e55d5a32b7939910
Mitesh <dev.miteshgodne@gmail.com>
2024-07-24 01:39:28 +0530

    Fast Exponentiation Algorithm

--- _4_MathAlgorithms/FastExponentiation.java
+++ _4_MathAlgorithms/FastExponentiation.java
@@ -2,17 +2,20 @@ package _4_MathAlgorithms;
 
 import java.util.Scanner;
 
-public class FastExponentiation{
+public class FastExponentiation {
 
-  static void fastExponentiation(int num, int pow){
+  // TC = O(log n)
+  static void fastExponentiation(int num, int pow) {
     int res = 1;
-    while(pow>0){
-      if(pow%2!=0) res*=num;
-      pow/=2;
-      num*=num;
+    while (pow > 0) {
+      if (pow % 2 != 0)
+        res *= num;
+      pow /= 2;
+      num *= num;
     }
     System.out.print("Result of exponentiation is : " + res);
   }
+
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
       System.out.print("Enter the base number : ");
@@ -20,6 +23,8 @@ public class FastExponentiation{
       System.out.print("Enter the pow number : ");
       int pow = sc.nextInt();
       fastExponentiation(num, pow);
+    } catch (Exception e) {
+      System.out.println("\nInvalid Input !! Please restart the program and enter a valid input ...");
     }
   }
 }

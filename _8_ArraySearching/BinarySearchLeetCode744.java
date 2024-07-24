02b59a4c51b1bb2553741eb4c8e50dbe7f6e2420
Mitesh <dev.miteshgodne@gmail.com>
2024-07-25 02:42:50 +0530

    Find smallest character from character array that is lexicographically greater than target

---
+++ _8_ArraySearching/BinarySearchLeetCode744.java
@@ -0,0 +1,27 @@
+package _8_ArraySearching;
+
+// QUE.744 -  You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+// Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+public class BinarySearchLeetCode744 {
+  public static char nextGreatestLetter(char[] letters, char target) {
+    char res = letters[0];
+    int start = 0, end = letters.length - 1, mid;
+    while (start <= end) {
+      mid = start + (end - start) / 2;
+      if (letters[mid] <= target) {
+        start = mid + 1;
+      } else {
+        res = letters[mid];
+        end = mid - 1;
+      }
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    char[] letters = { 'c', 'f', 'h'};
+    char res = nextGreatestLetter(letters, 'c');
+    System.out.println(res);
+  }
+}

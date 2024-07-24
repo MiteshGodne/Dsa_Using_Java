4f4eac46b2c3e45a9a26c47ba7c91933d5cc5ebe
Mitesh <dev.miteshgodne@gmail.com>
2024-07-25 03:10:19 +0530

    Find smallest character from character array that is lexicographically greater than target

--- _8_ArraySearching/BinarySearchLeetCode744.java
+++ _8_ArraySearching/BinarySearchLeetCode744.java
@@ -1,6 +1,6 @@
 package _8_ArraySearching;
 
-// QUE.744 -  You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+// Que.744 -  You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 // Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
 public class BinarySearchLeetCode744 {
